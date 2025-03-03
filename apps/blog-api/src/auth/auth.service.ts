a4863dabfeecff0ce3005f0ad328ca5414ecce6c
caizhihao <caizhihao@suntang.com>
2025-03-03 19:00:31 +0800

    feat:开发登录、注册、jwt passport

---
+++ apps/blog-api/src/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { SignInInput } from './dto/signin-input';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { verify } from 'argon2';
+import { User } from '@prisma/client';
+import { AuthJwtPayload } from './types/auth-jwtPayload';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+  async validateLocalUser({ email, password }: SignInInput) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) throw new UnauthorizedException('User not found');
+
+    const matchedUser = await verify(user.password!, password);
+
+    if (!matchedUser) throw new UnauthorizedException('Invalid password');
+
+    return user;
+  }
+
+  async generateToken(userId: number) {
+    const payload: AuthJwtPayload = { sub: userId };
+    const accessToken = this.jwtService.sign(payload);
+    return { accessToken };
+  }
+
+  async login(user: User) {
+    const { accessToken } = await this.generateToken(user.id);
+    return {
+      id: user.id,
+      name: user.name,
+      avatar: user.avatar,
+      accessToken,
+    };
+  }
+}
---
+++ apps/blog-api/src/user/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { hash } from 'argon2';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+  async create(createUserInput: CreateUserInput) {
+    const { password, ...user } = createUserInput;
+    const hashedPassword = await hash(password);
+    return this.prisma.user.create({
+      data: {
+        password: hashedPassword,
+        ...user,
+      },
+    });
+  }
+}
